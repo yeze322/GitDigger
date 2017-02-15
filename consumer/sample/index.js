411aaab45aa1b8f71d10a368229e1b0d62a802e3
Ze Ye <zeye@microsoft.com>
2017-02-15 15:08:22 +0800

    add sample json data

---
+++ consumer/sample/index.js
@@ -0,0 +1,11 @@
+var repo = require('./repo.json')
+var user = require('./user.json')
+var starring = require('./starring.json')
+var stargazer = require('./stargazer.json')
+
+module.exports = {
+  repo,
+  user,
+  starring,
+  stargazer
+}
