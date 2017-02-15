22f84e657273b345a2cfb2f272f5a98d6f656f26
Ze Ye <zeye@microsoft.com>
2017-02-15 14:50:11 +0800

    update sql script

--- psql/createTable.sql
+++ psql/createTable.sql
@@ -1,15 +1,3 @@
-CREATE TABLE starring (
-  id INT PRIMARY KEY NOT NULL,
-  userid INT NOT NULL,
-  repolist JSON NOT NULL
-);
-
-CREATE TABLE stargazers (
-  id INT PRIMARY KEY NOT NULL,
-  repoid INT NOT NULL,
-  userlist JSON NOT NULL
-);
-
 CREATE TABLE users (
   id INT PRIMARY KEY NOT NULL,
   payload JSON NOT NULL
@@ -18,4 +6,18 @@ CREATE TABLE users (
 CREATE TABLE repos (
   id INT PRIMARY KEY NOT NULL,
   payload JSON NOT NULL
+);
+
+CREATE SEQUENCE starrings_id_seq;
+CREATE TABLE starrings (
+  id INT PRIMARY KEY DEFAULT nextval('starrings_id_seq'),
+  userid INT NOT NULL,
+  repolist JSON NOT NULL
+);
+
+CREATE SEQUENCE stargazers_id_seq;
+CREATE TABLE stargazers (
+  id INT PRIMARY KEY DEFAULT nextval('stargazers_id_seq'),
+  repoid INT NOT NULL,
+  userlist JSON NOT NULL
 );
---
+++ psql/dropTable.sql
@@ -0,0 +1,7 @@
+DROP TABLE users;
+DROP TABLE repos;
+DROP TABLE stargazers;
+DROP TABLE starrings;
+DROP SEQUENCE stargazers_id_seq;
+DROP SEQUENCE starrings_id_seq;
+
