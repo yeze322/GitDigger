fa6f90707a384cd506e6433aaafc1e5a5a9eb2af
Ze Ye <zeye@microsoft.com>
2017-02-19 14:39:09 +0800

    integrate db logic into executor

--- payload2db/executor.js
+++ payload2db/executor.js
@@ -1,29 +1,58 @@
-var DBClient = require('./pgpClient')
+var pgp = require('pg-promise')({
+  promiseLib: require('bluebird')
+})
+var db = pgp(require('./config.json'))
 var q = require('../messageQ')
 var { SAVE } = require('../actions/types')
 
-const onErr = (reason) => {
-  console.log('[db]Failed: ', reason.detail)
-}
+const getQuery = pgp.helpers.insert
+const onErr = (table) => (reason) => { console.log('[db]Failed at ', table, ' -> ', reason.detail) }
 
 console.log('================================')
-console.log('Starting PostgresQL client ...')
-var client = new DBClient()
-
 console.log('Register message listener ...')
+
 q.process(SAVE.REPO, function (job, done) {
-  client.saveRepo(job.data).catch(onErr).finally(done)
+  // console.log('[SAVE][REPO]: ', job.data.id, job.data.full_name)
+  var data = {
+    id: job.data.id,
+    payload: job.data
+  }
+  db.query(getQuery(data, ['id', 'payload'], 'repos'))
+    .catch(onErr('repos'))
+    .finally(done)
 })
+
 q.process(SAVE.STARGAZER, function (job, done) {
-  client.saveStargazer(job.data).catch(onErr).finally(done)
+  // console.log('[SAVE][STARGAZER]: ', job.data.repoid, 'count = ', job.data.userlist.length)
+  var data = {
+    repoid: job.data.repoid,
+    userlist: job.data.userlist
+  }
+  db.query(getQuery(data, ['repoid', 'userlist'], 'stargazers'))
+    .catch(onErr('stargazers'))
+    .finally(done)
 })
 
 q.process(SAVE.USER, function (job, done) {
-  client.saveUser(job.data).catch(onErr).finally(done)
+  // console.log('[SAVE][USER]: ', job.data.id, job.data.login)
+  var data = {
+    id: job.data.id,
+    payload: job.data
+  }
+  db.query(getQuery(data, ['id', 'payload'], 'users'))
+    .catch(onErr('users'))
+    .finally(done)
 })
 
 q.process(SAVE.STARRING, function (job, done) {
-  client.saveStarring(job.data).catch(onErr).finally(done)
+  // console.log('[SAVE][STARRING]: ', job.data.userid, 'count = ', job.data.repolist.length)
+  var data = {
+    userid: job.data.userid,
+    repolist: job.data.repolist
+  }
+  db.query(getQuery(data, ['userid', 'repolist'], 'starrings'))
+    .catch(onErr('starrings'))
+    .finally(done)
 })
 
 q.process(SAVE.BATCH_USERS, function (job, done) {
@@ -39,7 +68,7 @@ console.log('================================')
 
 function cleanUp () {
   console.log('Stop psql client ...')
-  client.end()
+  pgp.end()
   console.log('Stop kue connection ...')
   q.shutdown(3000, function (err) {
     console.log('Kue Shutdown: ', err || '')
