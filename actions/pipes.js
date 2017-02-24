72c4ad3fc991f46b1d3bc8f73dead804199daac6
Ze Ye <zeye@microsoft.com>
2017-02-24 16:26:53 +0800

    set default page_count

--- actions/pipes.js
+++ actions/pipes.js
@@ -2,16 +2,28 @@ let rp = require('request-promise')
 
 let repoName2repoUrl = (fullName) => `https://api.github.com/repos/${fullName}`
 let repo2stargazerUrl = (repo) => `https://api.github.com/repos/${repo.full_name}/stargazers`
-let user2starringUrl = (user) => `https://api.github.com/users/${user.login}/starred`
-let page2urlSuffix = (page, per_page) => `?page=${page}&per_page=${per_page}`
+let user2starringUrl = (user) => `https://api.github.com/users/${user.login}/starred?per_page=100`
+let _page2urlSuffix = (page, per_page) => `?page=${page}&per_page=${per_page}`
+
+let link2nextUrl = (link) => {
+  let re = /<(.+)>; rel="next"/
+  let match = re.exec(link)
+  if (match) {
+    return match[1]
+  }
+  return null
+}
 
 let repo2stargazerUrlList = (repo) => {
+  let prefix = repo2stargazerUrl(repo)
   let stargazerCnt = repo.stargazers_count
+  if (!stargazerCnt) {
+    return [prefix]
+  }
   let per_page = 100
   let ret = []
-  let prefix = repo2stargazerUrl(repo)
   for (let i = 0; i < stargazerCnt / per_page; i++) {
-    ret.push(prefix + page2urlSuffix(i, per_page))
+    ret.push(prefix + _page2urlSuffix(i, per_page))
   }
   return ret
 }
@@ -34,6 +46,7 @@ let _url2request = (url) => {
 
 var _testGraph = require('./testgraph.json')
 let _test_url2request = (url) => {
+  console.log('[GET]- ', url)
   return new Promise((resolve, reject) => {
     let value = _testGraph[url]
     if (value) {
@@ -45,9 +58,10 @@ let _test_url2request = (url) => {
 }
 
 module.exports = {
+  link2nextUrl,
   repoName2repoUrl,
   repo2stargazerUrl,
+  repo2stargazerUrlList,
   user2starringUrl,
-  page2urlSuffix,
   url2request: _test_url2request
 }
